c4a0bd06e7be7cee38be7a6970342ede68b76951
harelios <harel.zenou@gmail.com>
2025-03-29 18:33:03 +0300

    Added a password to access the menu

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,20 @@ using namespace std;
 
 
 int main()
-{
-	DisplayMenu();
+{	
+	string owner_password = "Admin100"; //Password of the owner to prevent anybody who are not allowed to access the menu (you can personalize it).
+	string user_input;
+	cout << "Enter the password to access the menu :" << endl;
+	cin >> user_input;
+	
+	if (user_input == owner_password)
+	{      
+		cout << "Access allowed." << endl;
+		DisplayMenu();
+	}
+	else
+	{
+		cout << "Access denied" << endl;
+	}
 
 }
